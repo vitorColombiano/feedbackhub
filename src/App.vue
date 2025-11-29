2597a588184f7ed5f8180c1c907b52791a35d52c
vitorColombiano <vicolombiano69@gmail.com>
2025-11-29 20:35:31 -0300

    refactor: enhance feedback handling and improve layout in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import type { Feedbacks } from './types/Feedbacks';
 import { feedbackOptions } from './consts/feedbackOptions';
 
@@ -9,6 +9,7 @@ const selectedType = ref<string>(feedbackOptions[0] ?? 'positive');
 
 const feedbackPositives = ref<Feedbacks[]>([]);
 const feedbackNegatives = ref<Feedbacks[]>([]);
+const hasFeedbacks = computed(() => feedbackList.value.length > 0);
 
 function submitFeedback() {
   window.alert('O seu feedback foi registrado');
@@ -25,12 +26,12 @@ function submitFeedback() {
 </script>
 
 <template>
-  <main>
+  <main class="min-h-screen flex flex-col justify-between">
     <header class="bg-green-300 text-center p-5">
       <h1 class="font-mono text-2xl">FeedbackHub - Organização de Feedbacks em um só lugar</h1>
       <p class="text-sm font-mono">Welcome to FeedbackHub!</p>
     </header>
-    <section class="m-10 p-5 border border-gray-300 rounded-lg text-center bg-white">
+    <section class="m-8 md:my-10 md:mx-28 p-5 border border-gray-300 rounded-lg text-center bg-white">
       <h2 class="font-mono text-xl">
         Esse é um projeto feito com vue 3 + vite + tailwindcss para organizar feedbacks de diversos produtos em um só lugar.
       </h2>
@@ -55,12 +56,12 @@ function submitFeedback() {
         </button>
       </form>
     </section>
-    <section class="m-10 grid grid-cols-2 gap-8">
+    <section v-if="hasFeedbacks" class="m-10 md:my-10 md:mx-28 grid grid-cols-1 md:grid-cols-2 gap-8">
       <div>
         <h2 class="font-mono text-xl mb-4">Feedbacks Positivos</h2>
         <div v-for="feedbackItem in feedbackPositives" class="border border-green-600 p-4 rounded-2xl mt-6 w-full bg-white">
           <p class="font-mono text-sm">
-            {{ feedbackItem.feedback }} / {{ feedbackItem.type }}
+            {{ feedbackItem.feedback }}
           </p>
         </div>
       </div>
@@ -68,7 +69,7 @@ function submitFeedback() {
         <h2 class="font-mono text-xl mb-4">Feedbacks Negativos</h2>
         <div v-for="feedbackItem in feedbackNegatives" class="border border-red-600 p-4 rounded-2xl mt-6 w-full bg-white">
           <p class="font-mono text-sm">
-            {{ feedbackItem.feedback }} / {{ feedbackItem.type }}
+            {{ feedbackItem.feedback }}
           </p>
         </div>
       </div>
